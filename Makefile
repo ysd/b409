eeec4aede5bde9fc043fb1ae9b6b2b06623b2375
zz chen <grantchen88@gmail.com>
2013-06-13 09:18:26 +0800

    f

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-link_lib = -L/usr/local/lib -ltokyocabinet -lz -lbz2 -lrt -lpthread -lm -lc
+link_lib = -L/usr/local/lib -ltokyocabinet -lz -lbz2 -lrt -lpthread -lm -lc -lxml2
 objects = md_func.o utility.o posix_api.o
 obj_main = _creat.o _read.o _write.o _remove.o super_serv_process.o 
 .PHONY : all _creat _read _write _remove super_serv_process x
@@ -14,7 +14,7 @@ _remove : _remove.o $(objects)
 super_serv_process : super_serv_process.o $(objects)
 	gcc super_serv_process.o $(objects) -g -o super_serv_process $(link_lib) 
 x :
-	gcc -I/usr/include/libxml2 nss.c name_buf.c namespace_server.c xml.c -o x -lpthread -lxml2
+	gcc -I/usr/include/libxml2 nss.c md5.c md_func.c utility.c name_buf.c namespace_server.c xml.c -o x $(link_lib)
 .PHONY : clean
 clean :
 	rm _creat _read _write _remove super_serv_process $(objects) $(obj_main) 
--- utility.h
+++ utility.h
@@ -1,14 +1,14 @@
 #ifndef _UTILITY_H
 #define _UTILITY_H
-_PROTOTYPE(int get_time_str,(char *p,time_t t));
-_PROTOTYPE(int get_port_str,(char *p,int port));
-_PROTOTYPE(int get_cache_path,(char* path,char* cache_path));
-_PROTOTYPE(int get_data_path,(char* path,char* data_path));
-_PROTOTYPE(int get_path_from_fd,(int fd, char* path));
-_PROTOTYPE(void rand_generator,(int a[],int len,int limit));
-_PROTOTYPE(void u64_to_str,(u64 i,char *buf,u32 bufsiz));
-_PROTOTYPE(void u32_to_str,(u32 i,char *buf,u32 bufsiz));
-_PROTOTYPE(void u8_to_str,(char ch,char *buf,u32 bufsiz));
-_PROTOTYPE(u64 atoi_u64,(char *p));
-_PROTOTYPE(u32 atoi_u32,(char *p));
+extern int get_time_str(char *p,time_t t);
+extern int get_port_str(char *p,int port);
+extern int get_cache_path(char* path,char* cache_path);
+extern int get_data_path(char* path,char* data_path);
+extern int get_path_from_fd(int fd, char* path);
+extern void rand_generator(int a[],int len,int limit);
+extern void u64_to_str(u64 i,char *buf,u32 bufsiz);
+extern void u32_to_str(u32 i,char *buf,u32 bufsiz);
+extern void u8_to_str(char ch,char *buf,u32 bufsiz);
+extern u64 atoi_u64(char *p);
+extern u32 atoi_u32(char *p);
 #endif
