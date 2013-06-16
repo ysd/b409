786d9c4789fca419395c03da6ebd7bb91a458497
root <root@supercache.(none)>
2013-06-16 10:08:26 +0800

    start to add io_api

---
+++ io_api.c
@@ -0,0 +1,7 @@
+/**/
+int _creat(char * file_name_finger_print)
+{
+}
+int _read(char * file_name_finger_print,u8 * buf,u32 count,u64 offset);
+int _write(char * file_name_finger_print,u8 * buf,u32 count,u64 offset);
+int _rm(char * file_name_finger_print);
