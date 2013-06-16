90f4bac3009b9acf793bf291b8c5f1894b70c1af
zz chen <grantchen88@gmail.com>
2013-06-16 21:12:30 +0800

    need more thinking..

--- io_api.h
+++ io_api.h
@@ -1,8 +1,8 @@
 #ifndef _IO_API_H
 #define _IO_API_H
 #include"global.h"
-extern int _creat(char * file_name_finger_print);
-extern int _read(char * file_name_finger_print,u8 * buf,u32 count,u64 offset);
-extern int _write(char * file_name_finger_print,u8 * buf,u32 count,u64 offset);
-extern int _rm(char * file_name_finger_print);
+extern int _creat(char * file_name);
+extern int _read(char * file_name,u8 * buf,u32 count,u64 offset);
+extern int _write(char * file_name,u8 * buf,u32 count,u64 offset);
+extern int _rm(char * file_name);
 #endif
